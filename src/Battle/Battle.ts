d5c57900e6ded5dc97c532c49324ec8512478768
Willian Felipe <w.felipebraz@gmail.com>
2023-05-09 19:35:32 -0300

    fix do 6, e req 7

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -3,7 +3,7 @@ import Fighter from '../Fighter/index';
 abstract class Battle {
   constructor(protected player: Fighter) { }
 
-//   Should return 1 if player wins, -1 otherwise
+  //   Should return 1 if player wins, -1 otherwise
   fight(): number {
     return this.player.lifePoints === -1 ? -1 : 1;
   }
